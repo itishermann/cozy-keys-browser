426091cf26d1add65e62ea1b0d2f97a5ca8cf8a5
Ben <mail1@sonadresse.com>
2020-11-28 18:36:03 +0100

    add directories - all ciphers list not removed

--- src/popup/vault/groupings.component.ts
+++ src/popup/vault/groupings.component.ts
@@ -18,6 +18,8 @@ import { BrowserApi } from '../../browser/browserApi';
 import { CipherType } from 'jslib/enums/cipherType';
 
 import { CipherView } from 'jslib/models/view/cipherView';
+// import { CollectionView } from 'jslib/models/view/collectionView';
+import { FolderView } from 'jslib/models/view/folderView';
 
 import { CipherService } from 'jslib/abstractions/cipher.service';
 import { CollectionService } from 'jslib/abstractions/collection.service';
@@ -51,6 +53,10 @@ const ScopeStateId = ComponentId + 'Scope';
  */
 export class GroupingsComponent extends BaseGroupingsComponent implements OnInit, OnDestroy {
     ciphers: CipherView[];
+    favoriteCiphers: CipherView[];
+    noFolderCiphers: CipherView[];
+    folderCounts = new Map<string, number>();
+    collectionCounts = new Map<string, number>();
     typeCounts = new Map<CipherType, number>();
     searchText: string;
     state: any;
@@ -81,6 +87,15 @@ export class GroupingsComponent extends BaseGroupingsComponent implements OnInit
         private location: Location) {
         super(collectionService, folderService, storageService, userService);
         this.noFolderListSize = 100;
+    }
+
+    get showNoFolderCiphers(): boolean {
+        return this.noFolderCiphers != null && this.noFolderCiphers.length < this.noFolderListSize &&
+            this.collections.length === 0;
+    }
+
+    get folderCount(): number {
+        return this.nestedFolders.length - (this.showNoFolderCiphers ? 0 : 1);
     }
 
     async ngOnInit() {
@@ -152,22 +167,87 @@ export class GroupingsComponent extends BaseGroupingsComponent implements OnInit
     async load() {
         await super.load(false);
         await this.loadCiphers();
+        if (this.showNoFolderCiphers && this.nestedFolders.length > 0) {
+            // Remove "No Folder" from folder listing
+            this.nestedFolders = this.nestedFolders.slice(0, this.nestedFolders.length - 1);
+        }
+
         super.loaded = true;
     }
 
+    // async loadCiphers() {
+    //     this.allCiphers = await this.cipherService.getAllDecrypted();
+    //     this.deletedCount = this.allCiphers.filter((c) => c.isDeleted).length;
+    //     await this.search(null);
+    //     const typeCounts = new Map<CipherType, number>();
+    //     this.ciphers.forEach((c) => {
+    //         if (c.isDeleted) {
+    //             return;
+    //         }
+    //         if (typeCounts.has(c.type)) {
+    //             typeCounts.set(c.type, typeCounts.get(c.type) + 1);
+    //         } else {
+    //             typeCounts.set(c.type, 1);
+    //         }
+    //     });
+    //     this.ciphersByType = {};
+    //     this.ciphersByType[CipherType.Card] = this._ciphersByType(CipherType.Card);
+    //     this.ciphersByType[CipherType.Identity] = this._ciphersByType(CipherType.Identity);
+    //     this.ciphersByType[CipherType.Login] = this._ciphersByType(CipherType.Login);
+    //
+    //     this.typeCounts = typeCounts;
+    // }
+
     async loadCiphers() {
         this.allCiphers = await this.cipherService.getAllDecrypted();
+        if (!this.hasLoadedAllCiphers) {
+            this.hasLoadedAllCiphers = !this.searchService.isSearchable(this.searchText);
+        }
         this.deletedCount = this.allCiphers.filter((c) => c.isDeleted).length;
         await this.search(null);
+        let favoriteCiphers: CipherView[] = null;
+        let noFolderCiphers: CipherView[] = null;
+        const folderCounts = new Map<string, number>();
+        const collectionCounts = new Map<string, number>();
         const typeCounts = new Map<CipherType, number>();
         this.ciphers.forEach((c) => {
             if (c.isDeleted) {
                 return;
             }
+            if (c.favorite) {
+                if (favoriteCiphers == null) {
+                    favoriteCiphers = [];
+                }
+                favoriteCiphers.push(c);
+            }
+
+            if (c.folderId == null) {
+                if (noFolderCiphers == null) {
+                    noFolderCiphers = [];
+                }
+                noFolderCiphers.push(c);
+            }
+
             if (typeCounts.has(c.type)) {
                 typeCounts.set(c.type, typeCounts.get(c.type) + 1);
             } else {
                 typeCounts.set(c.type, 1);
+            }
+
+            if (folderCounts.has(c.folderId)) {
+                folderCounts.set(c.folderId, folderCounts.get(c.folderId) + 1);
+            } else {
+                folderCounts.set(c.folderId, 1);
+            }
+
+            if (c.collectionIds != null) {
+                c.collectionIds.forEach((colId) => {
+                    if (collectionCounts.has(colId)) {
+                        collectionCounts.set(colId, collectionCounts.get(colId) + 1);
+                    } else {
+                        collectionCounts.set(colId, 1);
+                    }
+                });
             }
         });
         this.ciphersByType = {};
@@ -175,7 +255,11 @@ export class GroupingsComponent extends BaseGroupingsComponent implements OnInit
         this.ciphersByType[CipherType.Identity] = this._ciphersByType(CipherType.Identity);
         this.ciphersByType[CipherType.Login] = this._ciphersByType(CipherType.Login);
 
+        this.favoriteCiphers = favoriteCiphers;
+        this.noFolderCiphers = noFolderCiphers;
         this.typeCounts = typeCounts;
+        this.folderCounts = folderCounts;
+        this.collectionCounts = collectionCounts;
     }
 
     async search(timeout: number = null) {
@@ -212,6 +296,16 @@ export class GroupingsComponent extends BaseGroupingsComponent implements OnInit
 
     countByType(type: CipherType) {
         return this.typeCounts.get(type);
+    }
+
+    async selectType(type: CipherType) {
+        super.selectType(type);
+        this.router.navigate(['/ciphers'], { queryParams: { type: type } });
+    }
+
+    async selectFolder(folder: FolderView) {
+        super.selectFolder(folder);
+        this.router.navigate(['/ciphers'], { queryParams: { folderId: folder.id || 'none' } });
     }
 
     async selectTrash() {
